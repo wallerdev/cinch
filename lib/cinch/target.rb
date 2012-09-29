8a43c73510f60f4315d0e6e41f63d0cd939a2627
Edward Waller <edward@wallerdev.com>
2012-09-29 04:01:32 -0300

    Update lib/cinch/target.rb
    Removed line splitting due to not having support on twitch.tv.

--- lib/cinch/target.rb
+++ lib/cinch/target.rb
@@ -34,28 +34,7 @@ module Cinch
       command = notice ? "NOTICE" : "PRIVMSG"
 
       text.split(/\r\n|\r|\n/).each do |line|
-        maxlength = 510 - (":" + " #{command} " + " :").size
-        maxlength = maxlength - @bot.mask.to_s.length - @name.to_s.length
-        maxlength_without_end = maxlength - split_end.bytesize
-
-        if line.bytesize > maxlength
-          splitted = []
-
-          while line.bytesize > maxlength_without_end
-            pos = line.rindex(/\s/, maxlength_without_end)
-            r = pos || maxlength_without_end
-            splitted << line.slice!(0, r) + split_end.tr(" ", "\u00A0")
-            line = split_start.tr(" ", "\u00A0") + line.lstrip
-          end
-
-          splitted << line
-          splitted[0, (@bot.config.max_messages || splitted.size)].each do |string|
-            string.tr!("\u00A0", " ") # clean string from any non-breaking spaces
-            @bot.irc.send("#{command} #@name :#{string}")
-          end
-        else
-          @bot.irc.send("#{command} #@name :#{line}")
-        end
+        @bot.irc.send("#{command} #@name :#{line}")
       end
     end
     alias_method :send, :msg
